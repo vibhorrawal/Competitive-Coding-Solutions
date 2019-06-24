99fbc657acd5fad4270cab0c6dbb0c0384eb3ac0
tokirmanva22 <38378125+tokirmanva22@users.noreply.github.com>
2019-06-24 21:11:31 +0530

    Add files via upload

---
+++ Max-Min_Hacker_Rank.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maxMin function below.
+def maxMin(k, arr):
+    ans = 10**9
+    arr_s = sorted(arr)
+    for i in range(len(arr) - k + 1):
+        sub_arr_min = arr_s[i]
+        sub_arr_max = arr_s[i+k-1]
+        ans = min(sub_arr_max - sub_arr_min, ans)
+    return ans
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    k = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
